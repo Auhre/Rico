c43435f813f17c2073547d29a956b8c5930a349c
Auhre <reyricollona.alzaga@bicol-u.edu.ph>
2022-07-15 16:18:36 +0800

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rico <code>src/App.js</code> rico rico rico ri.
+          Rico <code>src/App.js</code> rico rico.
         </p>
         <a
           className="App-link"
